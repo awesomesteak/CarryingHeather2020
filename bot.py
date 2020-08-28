df475b7b847831b496f46be40e6b4a180f42894a
Connor Sonnier <connor.sonnier@carvana.com>
2020-08-27 22:02:52 -0700

    Removed more unused code

--- bot.py
+++ bot.py
@@ -15,9 +15,6 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_Step
 MOTOR_X_REVERSED = False
 MOTOR_Y_REVERSED = False
 
-MAX_STEPS_X = 30
-MAX_STEPS_Y = 15
-
 RELAY_PIN = 22
 
 #######################
